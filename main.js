348d057c0e4701a97a6fce712e8b0c849e0fd827
lautili <lautilizardi@gmail.com>
2025-05-21 13:51:25 -0300

    Segundo commit

--- main.js
+++ main.js
@@ -1,19 +1,6 @@
-let color='blanco';
 function handleClick(e){
     let cuadrado=e.target;
-    if (cuadrado.classList.contains('rojo')){
-        color = 'red';
-    }else if (cuadrado.classList.contains('verde')){
-        color = 'green';
-    }else if (cuadrado.classList.contains('amarillo')){
-        color = 'yellow';
-    }else if (cuadrado.classList.contains('negro')){
-        color = 'black';
-    }else if (cuadrado.classList.contains('azul')){
-        color = 'blue';
-    }else if (cuadrado.classList.contains('blanco')){
-        color = 'white';
-    }
+    let color = document.querySelector('input').value;
     if (cuadrado.classList.contains('cuadrado')){
         cuadrado.style.background = color;
     }
